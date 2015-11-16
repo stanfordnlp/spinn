19faaae7a76b896dccec48fb9fdee98c0e5bfab2
Sam Bowman <sbowman@stanford.edu>
2015-11-15 18:40:40 -0800

    Split make_sweep into SNLI/SST versions.

---
+++ make_snli_sweep.py
@@ -0,0 +1,91 @@
+# Create a script to run a random hyperparameter search.
+
+import copy
+import random
+import numpy as np
+
+LIN = "LIN"
+EXP = "EXP"
+
+# Instructions: Configure the variables in this block, then run
+# the following on a machine with qsub access:
+# python make_sweep.py > my_sweep.sh
+# bash my_sweep.sh
+
+# - #
+
+# Non-tunable flags that must be passed in.
+
+FIXED_PARAMETERS = {
+    "data_type":     "snli",
+    "model_type":     "Model0",
+    "training_data_path":    "../data/snli_1.0_train.jsonl",
+    "eval_data_path":    "../data/snli_1.0_dev.jsonl",
+    "embedding_data_path": "/scr/nlp/data/glove_vecs/glove.840B.300d.txt",
+    "word_embedding_dim":	"300",
+    "model_dim":   "100",
+    "seq_length":	"50",
+    "eval_seq_length":	"100",
+    "clipping_max_norm":  "5.0",
+    "batch_size":  "128",
+}
+
+# Tunable parameters.
+SWEEP_PARAMETERS = {
+    "learning_rate":      (EXP, 0.00005, 0.0003),
+    "l2_lambda":   		  (EXP, 5e-7, 1e-4),
+    "init_range":         (EXP, 0.001, 0.005),
+    "double_identity_init_range": (EXP, 0.0005, 0.005),
+    "semantic_classifier_keep_rate": (LIN, 0.66, 1.0),
+    "embedding_keep_rate": (LIN, 0.5, 1.0)
+}
+
+sweep_name = "sweep_" + \
+    FIXED_PARAMETERS["data_type"] + "_" + FIXED_PARAMETERS["model_type"]
+sweep_runs = 6
+queue = "jag"
+
+# - #
+print "# NAME: " + sweep_name
+print "# NUM RUNS: " + str(sweep_runs)
+print "# SWEEP PARAMETERS: " + str(SWEEP_PARAMETERS)
+print "# FIXED_PARAMETERS: " + str(FIXED_PARAMETERS)
+print
+
+for run_id in range(sweep_runs):
+    params = {}
+    params.update(FIXED_PARAMETERS)
+    for param in SWEEP_PARAMETERS:
+        config = SWEEP_PARAMETERS[param]
+        t = config[0]
+        mn = config[1]
+        mx = config[2]
+
+        r = random.uniform(0, 1)
+        if t == EXP:
+            lmn = np.log(mn)
+            lmx = np.log(mx)
+            sample = np.exp(lmn + (lmx - lmn) * r)
+        else:
+            sample = mn + (mx - mn) * r
+
+        if isinstance(mn, int):
+            sample = int(round(sample, 0))
+
+        params[param] = sample
+
+    name = sweep_name + "_" + str(run_id)
+    flags = ""
+    for param in params:
+        value = params[param]
+        val_str = ""
+        flags += " --" + param + " " + str(value)
+        if param not in FIXED_PARAMETERS:
+            if isinstance(value, int):
+                val_disp = str(value)
+            else:
+                val_disp = "%.2g" % value
+            name += "-" + param + val_disp
+    flags += " --experiment_name " + name
+    print "export REMBED_FLAGS=\"" + flags + "\"; qsub -v REMBED_FLAGS train_rembed_classifier.sh -q " + queue
+    print
