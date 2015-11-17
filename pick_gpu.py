927c966ba1e18d8464caaded42ab49d59b3bcad7
Sam Bowman <sbowman@stanford.edu>
2015-11-16 20:08:06 -0800

    More pick_gpu cleanup.

--- pick_gpu.py
+++ pick_gpu.py
@@ -19,20 +19,19 @@ proc = subprocess.Popen("nvidia-smi", stdout=subprocess.PIPE,
 output, error = proc.communicate()
 if error:
     sys.stderr.write()
-    print "cpu"
+    sys.stdout.write("cpu")
 
 usage_re = re.compile(r"(?<=   )\d{1,3}(?=%)")
 matches = usage_re.findall(output)
 usage_amts = [int(usage_amt) for usage_amt in matches]
-print usage_amts
 
 # Get GPU
 gpus = [index for index in range(len(usage_amts))]
 open_gpus = [index for index in range(len(usage_amts)) if usage_amts[index] < USAGE_THRESHOLD]
 
 if open_gpus:
-    print "gpu" + str(random.choice(open_gpus))
+    sys.stdout.write("gpu" + str(random.choice(open_gpus)))
 elif gpus:
-    print "gpu" + str(random.choice(gpus))
+    sys.stdout.write("gpu" + str(random.choice(gpus)))
 else:
-    print "cpu"
+    sys.stdout.write("cpu")
