87467cf9718618c916d8ee6f2fb8ed689e73e6bc
Sam Bowman <sbowman@stanford.edu>
2015-11-21 10:44:19 -0800

    Better comments on the training script.

--- train_rembed_classifier.sh
+++ train_rembed_classifier.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 ### Generic job script for all experiments.
-# TODO(SB): Figure out how/if this interacts with Theano GPU use.
-
-#PBS -l nodes=1:ppn=4 	### Request at least 6 cores
-#PBS -l walltime=99:00:00	### Die after four days
-#PBS -l mem=4000MB
-#PBS -q nlp
 
 # Usage example:
 # export REMBED_FLAGS="--learning_rate 0.2"; qsub -v REMBED_FLAGS run.sh
+
+### The below are not comments, but actual PBS flags:
+#PBS -l walltime=99:00:00	### Die after four days
+#PBS -l mem=4000MB   ### Request 4G RAM
+#PBS -q jag   ### Run on the jagupard* queue
 
 # Change to the submission directory.
 cd $PBS_O_WORKDIR
