9f7d5e790ea2e913f213f8f82ad8a987ca28f27b
Sam Bowman <sbowman@stanford.edu>
2015-12-03 13:02:58 -0800

    Switch pick_gpu to use a memory, and to use a hard cutoff

--- pick_gpu.py
+++ pick_gpu.py
@@ -12,7 +12,7 @@ import re
 #
 # Maintainer: sbowman@stanford.edu
 
-USAGE_THRESHOLD = 90
+USAGE_THRESHOLD = 0.8
 
 proc = subprocess.Popen("nvidia-smi", stdout=subprocess.PIPE, 
                                       stderr=subprocess.PIPE)
@@ -21,17 +21,20 @@ if error:
     sys.stderr.write()
     sys.stdout.write("cpu")
 
-usage_re = re.compile(r"(?<=   )\d{1,3}(?=%)")
+usage_re = re.compile(r"(?<= )\d{1,8}(?=MiB /)")
 matches = usage_re.findall(output)
 usage_amts = [int(usage_amt) for usage_amt in matches]
 
-# Get GPU
-gpus = [index for index in range(len(usage_amts))]
-open_gpus = [index for index in range(len(usage_amts)) if usage_amts[index] < USAGE_THRESHOLD]
+total_re = re.compile(r"(?<=/)\s*\d{1,8}(?=MiB)")
+matches = total_re.findall(output)
+total_amts = [int(total) for total in matches]
+
+pct_used = [float(usage_amt)/float(total) for (usage_amt, total) in zip(usage_amts, total_amts)]
+print pct_used
+
+open_gpus = [index for index in range(len(pct_used)) if pct_used[index] < USAGE_THRESHOLD]
 
 if open_gpus:
     sys.stdout.write("gpu" + str(random.choice(open_gpus)))
-elif gpus:
-    sys.stdout.write("gpu" + str(random.choice(gpus)))
 else:
     sys.stdout.write("cpu")
