682258ee690d55c10c95f3969705ac80b3edad90
Jon Gauthier <jgauthie@stanford.edu>
2015-12-18 12:25:41 -0800

    Training script: use my virtualenv on the NLP cluster

--- train_rembed_classifier.sh
+++ train_rembed_classifier.sh
@@ -15,4 +15,7 @@ echo Device: $DEVICE
 # Log what we're running and where.
 echo $PBS_JOBID - `hostname` - $DEVICE - at `git log --pretty=format:'%h' -n 1` - $REMBED_FLAGS >> ~/rembed_machine_assignments.txt
 
+# Use Jon's Theano install.
+source /afs/cs.stanford.edu/u/jgauthie/scr/tmp/env-common/bin/activate
+
 THEANO_FLAGS=allow_gc=False,cuda.root=/usr/bin/cuda,warn_float64=warn,device=$DEVICE,floatX=float32 python -m rembed.models.classifier $REMBED_FLAGS
