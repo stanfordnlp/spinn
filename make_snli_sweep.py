ae3d0da893ec235847a0379c5654aa45535e4629
Sam Bowman <sbowman@stanford.edu>
2016-01-05 10:16:08 -0800

    Param update.

--- make_snli_sweep.py
+++ make_snli_sweep.py
@@ -37,7 +37,7 @@ FIXED_PARAMETERS = {
 
 # Tunable parameters.
 SWEEP_PARAMETERS = {
-    "learning_rate":      (EXP, 0.0005, 0.005),  # RNN likes higher end of range.
+    "learning_rate":      (EXP, 0.0005, 0.005),  # RNN likes higher end of range, but below 009.
     "l2_lambda":   		  (EXP, 5e-7, 2e-5),
     "semantic_classifier_keep_rate": (LIN, 0.85, 1.0),  # NB: Keep rates may depend considerably on dims.
     "embedding_keep_rate": (LIN, 0.75, 1.0),
