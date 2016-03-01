f993c8030aaa499fd883c0c838318a6601aa63d7
Jon Gauthier <jgauthie@stanford.edu>
2016-02-29 20:28:58 -0800

    Smaller batch size -- fat_stack can't handle that big

--- compare_fat_thin.sh
+++ compare_fat_thin.sh
@@ -2,7 +2,7 @@ for classifier in fat_classifier classifier; do
     export THEANO_FLAGS=compiledir=/tmp/theano.timing,device=gpu3,floatX=float32
     python -m rembed.models.${classifier} --eval_seq_length 50 --init_range 0.005 \
         --model_dim 300 --word_embedding_dim 300 \
-        --data_type bl --learning_rate 0.0006 --batch_size 512 --training_steps 100 \
+        --data_type bl --learning_rate 0.0006 --batch_size 256 --training_steps 100 \
         --tracking_lstm_hidden_dim 34 --seq_length 50 --model_type Model0 \
         --l2_lambda 1.06503778953e-06 --experiment_name time_${classifier} \
         --nolstm_composition --nouse_tracking_lstm --noconnect_tracking_comp \
