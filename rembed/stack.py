afada59a054f515e2e94f20b0138c76bb0045300
Sam Bowman <sbowman@stanford.edu>
2016-03-01 18:15:00 -0800

    Fix, take 2.

--- rembed/stack.py
+++ rembed/stack.py
@@ -475,7 +475,8 @@ class ThinStack(object):
         input_ndim += [len(extra_output_shape) + 1
                        for extra_output_shape in self.recurrence.extra_outputs]
 
-        wrt = self._vs.trainable_vars
+        wrt = wrt = [self._vs.vars[key] for key in self._vars
+               if key in self._vs.trainable_vars]
 
         # TODO handle gradient of action prediction
         # TODO would it pay off to force these to have the same concrete
