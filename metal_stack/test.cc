6ec434305dab807dc5fd479277eebe2356f89e00
Jon Gauthier <jon@gauthiers.net>
2016-03-10 16:56:08 -0800

    Start of a basic test harness

---
+++ metal_stack/test.cc
@@ -0,0 +1,36 @@
+#include <cuda_runtime.h>
+#include "cublas_v2.h"
+
+#include "thin-stack.h"
+#include "util.h"
+
+
+ThinStackParameters load_params(ModelSpec spec) {
+  float *compose_W_l = load_weights_cuda("params/compose_W_l.txt",
+      spec.model_dim ** 2);
+  float *compose_W_r = load_weights_cuda("params/compose_W_r.txt",
+      spec.model_dim ** 2);
+
+  ThinStackParameters ret = {
+    NULL, NULL, // projection
+    NULL, NULL, // buffer batch-norm
+    NULL, NULL, NULL, // tracking
+    compose_W_l, compose_W_r, NULL, NULL // composition
+  };
+
+  return ret;
+}
+
+int main() {
+  ModelSpec spec = {5, 5, 2, 10, 3, 5};
+  ThinStackParameters params = load_params(spec);
+
+  cublasHandle_t handle;
+  cublasStatus_t stat = cublasCreate(&handle);
+  if (stat != CUBLAS_STATUS_SUCCESS) {
+    printf("CUBLAS initialization failed\n");
+    return 1;
+  }
+
+  ThinStack ts(spec, params, handle);
+}
--- metal_stack/util.h
+++ metal_stack/util.h
@@ -3,6 +3,12 @@
 
 #include <cstddef>
 #include <iostream>
+#include <fstream>
+
+#include <cuda_runtime.h>
+
+using namespace std;
+
 
 typedef struct ModelSpec {
   size_t model_dim;
@@ -12,5 +18,27 @@ typedef struct ModelSpec {
   size_t seq_length;
   size_t model_visible_dim;
 } ModelSpec;
+
+
+float* load_weights(string filename, int N) {
+  float *ret = (float *) malloc(N * sizeof(float));
+  ifstream file(filename);
+
+  float x;
+  for (int i = 0; i < N; i++) {
+    file >> x;
+    cout << x << endl;
+    ret[i] = x;
+  }
+}
+
+float *load_weights_cuda(string filename, int N) {
+  float *h_weights = load_weights(filename, N);
+  float *d_weights;
+  cudaMalloc(&d_weights, N * sizeof(float));
+  cudaMemcpy(d_weights, h_weights, N * sizeof(float),
+      cudaMemcpyHostToDevice);
+  return d_weights;
+}
 
 #endif
