37c02499bbf223603387c8531cffe226c82ddf6f
Jon Gauthier <jgauthie@stanford.edu>
2016-05-04 11:26:29 -0700

    clean up stacktest.cc a bit

--- cpp/bin/stacktest.cc
+++ cpp/bin/stacktest.cc
@@ -1,3 +1,23 @@
+/**
+ * This binary runs a basic speed test of the C++ thin-stack implementation
+ * of SPINN in this package.
+ *
+ * The SPINN model and the thin-stack algorithm are described in our paper:
+ *
+ *     A Fast Unified Model for Sentence Parsing and Understanding.
+ *     Samuel R. Bowman, Jon Gauthier, Abhinav Rastogi, Raghav Gupta,
+ *     Christopher D. Manning, and Christopher Potts. arXiv March 2016.
+ *     http://arxiv.org/abs/1603.06021
+ *
+ * This script loads fixed parameters from the local `params` directory,
+ * generates random input data, and performs a SPINN feedforward as
+ * described in the paper. It outputs timing information and the final
+ * values at the top of the stack after the feedforward.
+ *
+ * For runtime instructions see the README in the root of this project
+ * directory.
+ */
+
 #include <chrono>
 #include <iostream>
 
@@ -30,7 +50,15 @@ void destroy_params(ThinStackParameters params) {
 }
 
 int main() {
-  ModelSpec spec = {300, 300, (size_t) atoi(getenv("BATCH_SIZE")), 10, 59, 300};
+  ModelSpec spec = {
+      300, // Dimension of stack values / constituent node values
+      300, // Word embedding dimension / tree leaf embedding dimension
+      (size_t) atoi(getenv("BATCH_SIZE")), // Batch size
+      10, // Vocabulary size
+      59, // Transition sequence length (== 2 * (sentence length) - 1)
+      300 // Ignore -- unused.
+  };
+
   ThinStackParameters params = load_params(spec);
 
   cublasHandle_t handle;
@@ -49,6 +77,10 @@ int main() {
 
   cout << "transitions:" << endl;
   float *h_transitions = (float *) malloc(spec.seq_length * spec.batch_size * sizeof(float));
+  // Build a batch of random transition sequences which somewhat resemble
+  // realistic transition sequences. (These are perhaps a bit more difficult
+  // than realistic transition sequence batches in that there will be less
+  // overlap w.r.t. merge locations within the sequence.)
   for (int i = 0; i < spec.seq_length * spec.batch_size; i++) {
     float val;
     if (i < spec.batch_size * 2) {
@@ -60,7 +92,9 @@ int main() {
     }
     h_transitions[i] = val;
   }
-  cudaMemcpy(ts.transitions, h_transitions, spec.seq_length * spec.batch_size * sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(ts.transitions, h_transitions,
+      spec.seq_length * spec.batch_size * sizeof(float),
+      cudaMemcpyHostToDevice);
   free(h_transitions);
 #if DEBUG
   print_device_matrix(ts.transitions, 1, spec.batch_size * spec.seq_length);
@@ -77,7 +111,9 @@ int main() {
 
   // Print the top of the stack.
   cout << "Stack top:" << endl;
-  print_device_matrix(&ts.stack[(spec.seq_length - 1) * spec.model_dim * spec.batch_size], spec.model_dim, spec.batch_size);
+  print_device_matrix(
+      &ts.stack[(spec.seq_length - 1) * spec.model_dim * spec.batch_size],
+      spec.model_dim, spec.batch_size);
 
   cout << "Total time elapsed: " << time_elapsed.count() << endl;
 
