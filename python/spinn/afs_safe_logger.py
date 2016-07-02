4fc292325722fb46e94d80a766faf0436087d003
Sam Bowman <sbowman@stanford.edu>
2016-07-02 16:30:01 -0700

    Add utility for writing JSON.

--- python/spinn/afs_safe_logger.py
+++ python/spinn/afs_safe_logger.py
@@ -1,6 +1,6 @@
 import datetime
 import sys
-
+import json
 
 class Logger(object):
     # A logging alternative that doesn't leave logs open between writes,
@@ -12,13 +12,14 @@ class Logger(object):
     WARNING = 2
     ERROR = 3
 
-    def __init__(self, log_path=None, min_print_level=0, min_file_level=0):
+    def __init__(self, log_path=None, json_log_path=None, min_print_level=0, min_file_level=0):
         # log_path: The full path for the log file to write. The file will be appended
         #   to if it exists.
         # min_print_level: Only messages with level above this level will be printed to stderr.
         # min_file_level: Only messages with level above this level will be
         #   written to disk.
         self.log_path = log_path
+        self.json_log_path = json_log_path
         self.min_print_level = min_print_level
         self.min_file_level = min_file_level
 
@@ -32,3 +33,11 @@ class Logger(object):
                 datetime_string = datetime.datetime.now().strftime(
                     "%y-%m-%d %H:%M:%S")
                 f.write("%s [%i] %s\n" % (datetime_string, level, message))
+
+    def LogJSON(self, message_obj, level=INFO):
+        if self.json_log_path and level >= self.min_file_level:
+            with open(self.json_log_path, 'w') as f:
+                print >>f, json.dumps(message_obj)
+        else:
+            sys.stderr.write('WARNING: No JSON log filename.')
+
