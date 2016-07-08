7431b9f0e0bf49b080311273509fb1ef810fe5a4
Jon Gauthier <jon@gauthiers.net>
2016-07-07 17:55:24 -0700

    definitively fix multinomial sampling problem

--- python/spinn/fat_stack.py
+++ python/spinn/fat_stack.py
@@ -301,11 +301,6 @@ class HardStack(object):
         # Sample parsing transitions according to predicted distribution.
         actions_t = T.nnet.softmax(actions_t)
         sampled = multinomial.MultinomialFromUniform("auto")(actions_t, uniform_t, 1)
-        # Very rarely MultinomialFromUniform will bork and return a row of all
-        # 0s. Guard against this.
-        # TODO: Find+fix the underlying problem..
-        sampled += 1. - T.max(sampled, axis=1)
-
         mask = sampled.nonzero()[1]
         mask = theano.gradient.disconnected_grad(mask)
 
@@ -429,7 +424,10 @@ class HardStack(object):
         # Sample from uniform distribution in advance. Sampling within _step
         # breaks things. We'll use the uniform values to draw from the multinomial
         # predicted during training.
-        uniform_samples = self.ss_mask_gen.uniform((self.seq_length, batch_size)) # HACK
+        uniform_samples = self.ss_mask_gen.uniform((self.seq_length, batch_size))
+        # Multinomial sampler will bork on input values at upper bound. Guard
+        # against this.
+        uniform_samples = T.min(uniform_samples, 0.99)
 
         # Prepare data to scan over.
         sequences = [transitions, uniform_samples]
